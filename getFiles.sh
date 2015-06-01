ff4152220645a77cf5c462c676862790a076a5b8
John Mercier <moaxcp@gmail.com>
2015-05-31 21:54:10 -0400

    adding kernel-config file

--- getFiles.sh
+++ getFiles.sh
@@ -1,3 +1,4 @@
 # copies files from my system to this repository. The files can then be committed with changes tracked.
 cp -r /home/john/.notion notion
 cp /etc/X11/xorg.conf .
+cp /usr/src/linux/.config kernel-config
