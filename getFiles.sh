16b396d4530f01b21f2377cfbb4a2880a1f4df27
John Mercier <moaxcp@gmail.com>
2015-06-27 16:53:03 -0400

    organizing getFiles.sh

--- getFiles.sh
+++ getFiles.sh
@@ -1,11 +1,20 @@
 # copies files from my system to this repository. The files can then be committed with changes tracked.
-cp -r /home/john/.notion notion
-cp /etc/X11/xorg.conf .
-cp /usr/src/linux/.config kernel-config
-cp /home/john/.xinitrc xinitrc
+#bash
 cp /home/john/.bashrc bashrc
-cp /etc/portage/make.conf make.conf
-cp /home/john/buildkernel.sh buildkernel.sh
+
+#notion
+cp -r /home/john/.notion notion
+
+#X11
+cp /etc/X11/xorg.conf .
+cp /home/john/.xinitrc xinitrc
 cp /home/john/.Xdefaults Xdefaults
 cp /home/john/.Xresources Xresources
+
+#portage
+cp /etc/portage/make.conf make.conf
+
+#kernel
+cp /usr/src/linux/.config kernel-config
+cp /home/john/buildkernel.sh buildkernel.sh
 
